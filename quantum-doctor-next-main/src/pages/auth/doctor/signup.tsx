d75bf7deaf1d4a862c0335b95b38133f8182fba4
SANDEEP KUMAR <143518095+skddl007@users.noreply.github.com>
2025-02-15 23:52:15 +0530

    Delete quantum-doctor-next-main directory

--- quantum-doctor-next-main/src/pages/auth/doctor/signup.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import CreateDoctorAccount from '../../../components/doctor/auth/createAccount/createAccount';
-
-export default function CreateDoctorAccountPage() {
-  return  <CreateDoctorAccount/>;
-}
-
--- quantum-doctor-next-main/src/pages/schedule/index.tsx
+++
@@ -1,9 +0,0 @@
-const Schedule = () => {
-    return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-2xl font-bold">Schedule</h1>
-        </div>
-    )
-}
-
-export default Schedule;
