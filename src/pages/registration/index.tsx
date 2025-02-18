05a71d73173352ddab248a70ef096dfdaaf80126
SANDEEP KUMAR <143518095+skddl007@users.noreply.github.com>
2025-02-18 22:16:11 +0530

    Delete src directory

--- src/pages/registration/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import RegistrationForm from '../../components/regProcess/RegistrationForm';
-
-export default function Registration() {
-  return <RegistrationForm />
-}
-
